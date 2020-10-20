b0f4bc1c3faedf997bee3bff43f9192d832512bb
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-20 09:16:07 +0300

    Write data in logcat

--- app/src/main/java/ru/mvlikhachev/retrofitdemo/MainActivity.java
+++ app/src/main/java/ru/mvlikhachev/retrofitdemo/MainActivity.java
@@ -3,12 +3,60 @@ package ru.mvlikhachev.retrofitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import ru.mvlikhachev.retrofitdemo.model.CountryInfo;
+import ru.mvlikhachev.retrofitdemo.model.Result;
+import ru.mvlikhachev.retrofitdemo.service.CountryService;
+import ru.mvlikhachev.retrofitdemo.service.RetrofitInstance;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Result> resultArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getCounties();
+    }
+
+    private Object getCounties() {
+
+        CountryService countryService = RetrofitInstance.getService();
+
+        Call<CountryInfo> call = countryService.getResults();
+
+        call.enqueue(new Callback<CountryInfo>() {
+            @Override
+            public void onResponse(Call<CountryInfo> call, Response<CountryInfo> response) {
+
+                CountryInfo countryInfo = response.body();
+                if (countryInfo != null && countryInfo.getRestResponse() != null) {
+                    resultArrayList =
+                            (ArrayList<Result>) countryInfo
+                                    .getRestResponse()
+                                    .getResult();
+
+                    Log.d("resultArrayList", "Countries:");
+                    for (Result result : resultArrayList) {
+                        Log.d("resultArrayList", result.getName());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<CountryInfo> call, Throwable t) {
+                Log.d("resultArrayList", "FAIL!!!: " + t);
+            }
+        });
+        return resultArrayList;
     }
 }
--- app/src/main/java/ru/mvlikhachev/retrofitdemo/service/RetrofitInstance.java
+++ app/src/main/java/ru/mvlikhachev/retrofitdemo/service/RetrofitInstance.java
@@ -1,5 +1,7 @@
 package ru.mvlikhachev.retrofitdemo.service;
 
+import android.util.Log;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -18,7 +20,6 @@ public class RetrofitInstance {
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
-
         return retrofit.create(CountryService.class);
     }
 }
